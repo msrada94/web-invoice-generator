f8829c57a567a0fea97abeb996cd1f50552627bb
Miguel Rada <mr33@sanger.ac.uk>
2025-09-11 13:55:06 +0100

    Update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,24 +17,22 @@ dependencies {
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-host-common")
     implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    implementation("io.ktor:ktor-server-core:2.3.6")
-    implementation("io.ktor:ktor-server-netty:2.3.6")
     implementation("io.ktor:ktor-server-html-builder:2.3.6")
     implementation("io.ktor:ktor-server-sessions:2.3.6")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.6")
     implementation("io.ktor:ktor-serialization-jackson:2.3.6")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.apache.poi:poi-ooxml:5.2.5")
     implementation("com.itextpdf:itext7-core:7.2.5")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
 tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
     archiveClassifier.set("all") // optional, usually default
     manifest {
-        attributes["Main-Class"] = "com.alicefield.ApplicationKt" // adjust as needed
+        attributes["Main-Class"] = "com.alicefield.ApplicationKt"
     }
 }
 
